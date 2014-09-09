012e52f03db7d478adbce0a1cb5cdf1aaddc7c5c
Michael Bauer <mihi@lo-res.org>
2014-09-09 11:14:18 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csv-map "0.1.1-SNAPSHOT"
+(defproject csv-map "0.1.1"
   :description "Convert well formatted CSV files to maps and vice versa"
   :url "http://github.com/mihi-tr/csv-map"
   :license {:name "Eclipse Public License"
